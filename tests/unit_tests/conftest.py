9990a930552c1c719ad5751d6c0b8850f8c34cc9
JimmyBoyle <jimmy@theboylefamily.net>
2018-11-02 14:52:13 -0700

    use os.cwd

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-my_path = os.path.abspath(__file__)
-sys.path.insert(0, my_path + '/../../../src/featuretoggles/')
+sys.path.insert(0, os.getcwd() + '/src/featuretoggles/')
 
 os.environ['PREFIX'] = 'testing'
